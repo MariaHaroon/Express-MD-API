25b2fd5be973bb6ef31a094b4155edb9e871b93e
Maria Haroon <mejoramiga.parasiempre@gmail.com>
2023-08-21 06:58:12 -0500

    Update index.jsx

--- client/src/guest/component/CustomForm/index.jsx
+++ client/src/guest/component/CustomForm/index.jsx
@@ -3,46 +3,46 @@ import './custom.css'
 import LoginForm from './LoginForm'
 import SignupForm from './SignupForm'
 
-// export default function CustomForm() {
-//     return (
-//         <div className="wrapper">
-//             <div className="card-switch">
-//                 <label className="switch">
-//                     <input type="checkbox" className="toggle" />
-//                     <span className="slider" />
-//                     <span className="card-side" />
-//                     <div className="flip-card__inner">
-//                         <LoginForm />
-//                         <Signupform />
-//                     </div>
-//                 </label>
-//             </div>
-//         </div>
-
-//     )
-// }
-
-
-
 export default function CustomForm() {
-    const [isSignup, setIsSignup] = useState(false);
-
-    const toggleForm = () => {
-        setIsSignup(!isSignup);
-    }
-
     return (
         <div className="wrapper">
             <div className="card-switch">
                 <label className="switch">
-                    <input type="checkbox" className="toggle" onChange={toggleForm} />
+                    <input type="checkbox" className="toggle" />
                     <span className="slider" />
                     <span className="card-side" />
                     <div className="flip-card__inner">
-                        {isSignup ? <SignupForm /> : <LoginForm />}
+                        <LoginForm />
+                        <Signupform />
                     </div>
                 </label>
             </div>
         </div>
+
     )
 }
+
+
+
+// export default function CustomForm() {
+//     const [isSignup, setIsSignup] = useState(false);
+
+//     const toggleForm = () => {
+//         setIsSignup(!isSignup);
+//     }
+
+//     return (
+//         <div className="wrapper">
+//             <div className="card-switch">
+//                 <label className="switch">
+//                     <input type="checkbox" className="toggle" onChange={toggleForm} />
+//                     <span className="slider" />
+//                     <span className="card-side" />
+//                     <div className="flip-card__inner">
+//                         {isSignup ? <SignupForm /> : <LoginForm />}
+//                     </div>
+//                 </label>
+//             </div>
+//         </div>
+//     )
+// }
